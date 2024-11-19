f001087e3dd86b9756bf700f4d473dd1e245d696
Anirudh Kadam <Anirudh.Kadam@teknopoint.local>
2024-11-19 13:37:38 +0530

    swiper lib files added

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,11 +1,9 @@
+import Swiper from "./swiper-bundle.min.js";
+
 export default function decorate(block) {
   const rows = Array.from(block.children);
   const config = rows[0]
   const items = rows.slice(1);
   const classes = items[0].firstElementChild.textContent.trim().split(",");
-  console.log(classes);
-  // classes.forEach(cls => {
-
-
-  // });
+  
 }
