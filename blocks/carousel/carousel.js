afe96b81f2eec4ab244cd8e383033df3f1837ab6
rahul.gohil1 <rahul.gohil@deptagency.com>
2024-11-22 12:37:32 +0530

    change in component definition

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,9 +1,8 @@
-import Swiper from "./swiper-bundle.min.js";
+// import Swiper from './swiper-bundle.min.js';
 
-export default function decorate(block) {
-  const rows = Array.from(block.children);
-  const config = rows[0]
-  const items = rows.slice(1);
-  const classes = items[0].firstElementChild.textContent.trim().split(",");
-  
-}
+// export default function decorate(block) {
+// const rows = Array.from(block.children);
+// const config = rows[0];
+// const items = rows.slice(1);
+// const classes = items[0].firstElementChild.textContent.trim().split(',');
+// }
